182299b75963d8e0713138c3ea91eb90e372111a
Luis Paulo Noronha <102933945+afcluispaulo@users.noreply.github.com>
2023-06-12 13:34:00 -0300

    adicionado rota params

--- Back-end aula/src/server.js
+++ Back-end aula/src/server.js
@@ -2,10 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
+app.get("/message", (request, response) => {
     response.send("Hello, world!");
     
 });
 
+// Route Params
+
+app.get("/message/:id/:user", (request, response) => {
+
+    response.send(`
+    Mensagem ID: ${request.params.id}.
+    Para o usuário: ${request.params.user}
+    `);
+    
+});
+
+
 const PORT = 3333;
-app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}. Link: https://localhost:${PORT}`));
