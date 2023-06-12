7037a6df7dc9a39bf7d94c14c28f94ebae90fcea
Luis Paulo Noronha <102933945+afcluispaulo@users.noreply.github.com>
2023-06-12 13:45:36 -0300

    adicionado query params

--- Back-end aula/src/server.js
+++ Back-end aula/src/server.js
@@ -2,22 +2,28 @@ const express = require("express");
 
 const app = express();
 
+// Método GET
 app.get("/message", (request, response) => {
     response.send("Hello, world!");
     
 });
 
-// Route Params
-
+// Route Params (usados para dados simples)
 app.get("/message/:id/:user", (request, response) => {
     const { id, user } = request.params
     response.send(`
-    Mensagem ID: ${id}.
-    Para o usuário: ${user}
+        Mensagem ID: ${id}.
+        Para o usuário: ${user}
     `);
     
 });
 
+app.get("/users", (request, response) => {
+    const { page, limit } = request.query;
+    
+    response.send(`Página: ${page}. Mostrar ${limit}`)
+    // localhost:3333/users?page=5&limit=10
+})
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}. Link: https://localhost:${PORT}`));
