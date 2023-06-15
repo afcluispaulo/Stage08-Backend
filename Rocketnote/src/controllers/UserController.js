f924e6ca76ce521df108f1b00094087d31f5d397
Luis Paulo Noronha <102933945+afcluispaulo@users.noreply.github.com>
2023-06-15 15:24:41 -0300

    adicionado status code

--- Rocketnote/src/controllers/UserController.js
+++ Rocketnote/src/controllers/UserController.js
@@ -10,8 +10,10 @@ class UserController {
     create(request, response) {
         const { name, email, password } = request.body;
     
-        response.json({ name, email, password });
+        response.status(201).json({ name, email, password });
     }
+
+
 }
 
 module.exports = UserController;
