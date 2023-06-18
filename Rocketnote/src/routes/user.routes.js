6bf87fd7a217ab5de30cef2762b97043ed741195
Luis Paulo Noronha <102933945+afcluispaulo@users.noreply.github.com>
2023-06-18 11:48:06 -0300

    adicionado classe de Erro

--- Rocketnote/src/routes/user.routes.js
+++ Rocketnote/src/routes/user.routes.js
@@ -7,13 +7,17 @@ const usersRoutes = Router();
 const usersController = new UserController;
 
 function myMiddlware(request, response, next) {
-    console.log("Você passou pelo Middlware!")
-    
+    console.log("Você passou pelo Middlware!");
+    console.log(request.body);
+
+    if(!request.body.isAdmin) {
+        return response.json({ message: "user unauthorized"});
+    }
+
+    // se não chamar o Next, o Insomnia rodará infinitamente
+    next();
+     
 }
-
-
-
-
 
 
 
