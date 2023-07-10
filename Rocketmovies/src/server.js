4889b627fbf9cafd025e6b66919c91f9735ed535
Luis Paulo Noronha <102933945+afcluispaulo@users.noreply.github.com>
2023-07-10 14:17:00 -0300

    criado conexão com sqlite3
    criado local do banco de dados, quando instalado

--- Rocketmovies/src/server.js
+++ Rocketmovies/src/server.js
@@ -1,8 +1,10 @@
-
+const database = require("./database/sqlite");
 const express = require("express");
 
 const app = express();
 app.use(express.json());
+
+database();
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}. Link: localhost:${PORT}`))
