b6171f0b75501a8711e87d5103cf6e26500b69cb
Luis Paulo Noronha <102933945+afcluispaulo@users.noreply.github.com>
2023-07-12 14:45:53 -0300

    criado controladores para MovieNotesController
    criado controladores de create, show e index

---
+++ Rocketmovies/src/controllers/MovieNotesController.js
@@ -0,0 +1,95 @@
+const knex = require("../database/knex");
+
+/**
+ * "title": "Introdução ao Nodejs",  
+ * "descriptions": "Essa é uma nota de exemplo",
+ * "tags": ["node", "express"]
+ */ 
+ 
+class MovieNotesController {
+    async create(request, response) {
+
+        // Nota: tem que ser igual ao que foi colocado em migrations
+        const { title, descriptions, tags } = request.body;
+        const { user_id }  = request.params;
+
+        const [ note_id ]  = await knex("notes").insert({
+            title,
+            descriptions,
+            user_id
+        });
+
+        const tagsInsert = tags.map(name => {
+            return {
+                note_id, 
+                name,
+                user_id
+            }
+        });
+
+        await knex("tags").insert(tagsInsert);
+
+       return response.json();
+    }
+
+    async show(request, response) {
+        const { id } = request.params;
+
+        const notes = await knex("notes").where({ id }).first();
+        const tags = await knex("tags").where({ note_id: id }).orderBy("name");
+
+        return response.json({
+            ...notes,
+            tags
+        });
+
+    }
+
+    async delete(request, response) { 
+        const { id } = request.params;
+
+        await knex("notes").where({ id }).delete();
+
+        return response.json();
+
+    }
+
+    async index(request, response) {
+        const { title,  user_id, tags } = request.query;
+
+        let notes;
+
+        if (tags) {
+            const filterTags = tags.split(',').map(tag => tag.trim());
+            console.log(filterTags)
+
+            notes = await knex("tags")
+            .select([
+                "notes.id",
+                "notes.title",
+                "notes.user_id",
+            ])
+            .where("notes.user_id", user_id)
+            .whereLike("notes.title", `%${title}%`)
+            .whereIn("name", filterTags) 
+            .innerJoin("notes", "notes.id", "tags.note_id")
+            .orderBy("notes.title")
+
+        }
+
+        const userTags = await knex("tags").where({ user_id })
+        const notesWithTags = await notes.map(note  => {
+            const notesTags = userTags.filter(tag => tag.note_id === note.id)
+
+            return {
+                ...note, 
+                tags: notesTags
+            }
+        })
+
+        return response.json(notesWithTags)
+    }
+
+}
+
+module.exports = MovieNotesController;
--- Rocketmovies/src/database/knex/migrations/20230712030842_createMovie_Notes.js
+++ Rocketmovies/src/database/knex/migrations/20230712030842_createMovie_Notes.js
@@ -1,7 +1,6 @@
 
 exports.up = knex => knex.schema.createTable("movie_notes", table => {
     table.increments("id");
-    
     table.text("title");
     table.text("descriptions");
 
