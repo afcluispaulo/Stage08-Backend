e573640ecf5eabdcc473019586db8f2d77d51037
Luis Paulo Noronha <102933945+afcluispaulo@users.noreply.github.com>
2023-07-12 00:14:16 -0300

    criado migration da tabela movie_notes

--- Rocketmovies/src/database/knex/migrations/20230712030842_createMovie_Notes.js
+++ Rocketmovies/src/database/knex/migrations/20230712030842_createMovie_Notes.js
@@ -1,15 +1,15 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+exports.up = knex => knex.schema.createTable("movie_notes", table => {
+    table.increments("id");
+    table.text("title");
+    table.text("descriptions");
+    table.integer("user_id").references("id").inTable("users");
+
+    table.timestamp("created_at").default(knex.fn.now());
+    table.timestamp("updated_at").default(knex.fn.now());
+
+
+});
+
+
+exports.down = knex => knex.schema.dropTable("movie_notes");
